057b7eaa8d3400fa168ae2737e70c9887267fe8c
Kykal <benavides.alan@hotmail.com>
2023-06-05 22:25:12 -0600

    Off limits on click handler

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -29,12 +29,42 @@ const Modal = (props: Modal): JSX.Element => {
 		document.body.removeAttribute('style');
 	}, [props.open] );
 
+
+	//Handlers
+	const _isBetween = ( min: number, target: number, max: number ): boolean => {
+		return min<=target && target<=max;
+	}
+
+	const _onClose = (event: React.MouseEvent<HTMLDialogElement>) => {
+		const current = ref.current!;
+		const yLimits = {
+			min: current.offsetTop!,
+			max: current.offsetTop! + current.offsetHeight!,
+		};
+		const xLimits = {
+			min: current.offsetLeft!,
+			max: current.offsetLeft! + current.offsetWidth!,
+		}
+
+		const isInsideX: boolean = _isBetween( xLimits.min, event.clientX, xLimits.max );
+		const isInsideY: boolean = _isBetween( yLimits.min, event.clientY, yLimits.max );
+
+		const isInsideDialog: boolean = isInsideX && isInsideY;
+
+		if( isInsideDialog ){
+			return;
+		}
+
+		props.onClose();
+	}
+
 	//Main component render
 	return (
 		<dialog
 			id={id}
 			ref={ref}
 			{...props}
+			onClick={_onClose}
 		>
 			{props.children}
 		</dialog>
